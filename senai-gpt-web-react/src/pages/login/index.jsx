9583085ae04b76c1bbea3933a6fef5782ed9bff1
Jaquelline <cjaquelliness@gmail.com>
2025-04-15 21:37:25 -0300

    importacao

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -3,11 +3,37 @@ import "./login.css";
 function Login() {
 
 
-    return (
-      <>
-        <h1>Login</h1>
-      </>
-    )
-  }
-  
-  export default Login;
+  return (
+    <>
+      <header></header>
+
+      <main className="paige-container">
+
+        <div className="robo-image"></div>
+
+
+        <div className="login-container">
+
+          <img className="logo" src="../assets/imgs/Chat.png" alt="Logo do SeniaGPT." />
+
+          <h1
+           
+            id="meutitulo"
+            className="titulo"
+          >Login</h1>
+
+          <input className="inpt" type="email" placeholder="Insira o e-mail" />
+          <input className="inpt" type="password" placeholder="Insira a senha" />
+
+          <button className="btn">Entrar</button>
+
+        </div>
+
+      </main>
+
+      <footer></footer>
+    </>
+  )
+}
+
+export default Login;
